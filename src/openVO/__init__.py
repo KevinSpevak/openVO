6c2a8d23d4dca441155169c91ffca086f7103bf3
Kevin Spevak <kevinspevak@gmail.com>
2022-03-30 22:00:04 -0600

    Initial imlementation of stereo visual odometry taken mainly from https://github.com/KevinSpevak/NUVO. Release version 0.0.3

---
+++ src/openVO/__init__.py
@@ -0,0 +1,5 @@
+# Allow importing classes/functions with "from openVO import ClassName/functionName"
+from .stereo_camera import StereoCamera
+from .stereo_odometer import StereoOdometer
+from .utils.rot2RPY import rot2RPY
+from .utils.drawPoseOnImage import drawPoseOnImage

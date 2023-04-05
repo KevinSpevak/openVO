e0caf5c0af9c598a27d11f3a409de88b686dee3c
justincdavis <davis.justin@mssm.org>
2023-04-05 15:48:02 -0600

    Finished OAK-D implementation

--- tests/oakd/odometer.py
+++ tests/oakd/odometer.py
@@ -1,7 +1,15 @@
+from threading import Thread
 import time
 
 from openVO.oakd import OAK_Odometer, OAK_Camera
 
+
+STOPPED = False
+def target():
+    global STOPPED
+    while not STOPPED:
+        print(odom.current_pose)
+        time.sleep(1)
 
 cam = OAK_Camera()
 cam.start()
@@ -13,6 +21,11 @@ odom = OAK_Odometer(cam)
 odom.start()
 odom.cam.start_display()
 
+t = Thread(target=target)
+t.start()
+
 input("Press Enter to continue...")
 
+STOPPED = True
+t.join()
 odom.stop()

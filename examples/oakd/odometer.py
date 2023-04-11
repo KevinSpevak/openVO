6c8d3c8c1bdf48c1176191c4f25760285a83a4ec
justincdavis <jcdavis@mssm.org>
2023-04-11 11:58:44 -0600

    Added standard openVO example
    Former-commit-id: 172a578f6dcb76409f452a891d20e80b835e7bb2

--- examples/oakd/odometer.py
+++ examples/oakd/odometer.py
@@ -7,6 +7,8 @@ from openVO.oakd import OAK_Odometer, OAK_Camera
 
 
 STOPPED = False
+
+
 def target():
     global STOPPED
     print(odom.cam._valid_region_left)
@@ -16,18 +18,26 @@ def target():
         img = odom.cam.rgb
         img = cv2.resize(img, (640, 480))
         drawPoseOnImage(T, img)
-        cv2.putText(img, odom.skip_cause, (200, 200), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+        cv2.putText(
+            img,
+            odom.skip_cause,
+            (200, 200),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            2,
+            (0, 0, 255),
+            3,
+        )
         cv2.imshow("Annotated", img)
         cv2.waitKey(33)
     cv2.destroyWindow("Annotated")
+
 
 cam = OAK_Camera()
 cam.start()
 
 time.sleep(5)
 
-odom = OAK_Odometer(cam,
-                    nfeatures=10000)
+odom = OAK_Odometer(cam, nfeatures=10000)
 
 odom.start()
 # odom.cam.start_display()

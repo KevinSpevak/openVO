a96beaaa327888f98e9b4abafd1a354f230c27d3
justincdavis <jcdavis@mssm.org>
2023-04-11 14:45:24 -0600

    Updated OAK_Odometer, pulled back from StereoOdometer, only change in compute_3d call in update (update params changed)

--- examples/oakd/odometer.py
+++ examples/oakd/odometer.py
@@ -1,52 +1,25 @@
-from threading import Thread
 import time
 
 import cv2
 from openVO import drawPoseOnImage
-from openVO.oakd import OAK_Odometer, OAK_Camera
+from openVO.oakd import OAK_Camera, OAK_Odometer
 
 
 STOPPED = False
 
 
-def target():
-    global STOPPED
-    print(odom.cam._valid_region_left)
-    print(odom.cam._valid_region_right)
-    while not STOPPED:
-        T = odom.current_pose
-        img = odom.cam.rgb
-        img = cv2.resize(img, (640, 480))
-        drawPoseOnImage(T, img)
-        cv2.putText(
-            img,
-            odom.skip_cause,
-            (200, 200),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            2,
-            (0, 0, 255),
-            3,
-        )
-        cv2.imshow("Annotated", img)
-        cv2.waitKey(33)
-    cv2.destroyWindow("Annotated")
-
-
 cam = OAK_Camera()
-cam.start()
+cam.start(block=True)
 
-time.sleep(5)
+odom = OAK_Odometer(cam)
 
-odom = OAK_Odometer(cam, nfeatures=10000)
+while True:
+    odom.update()
+    rgb_frame = cam.rgb
+    drawPoseOnImage(odom.current_pose(), rgb_frame)
+    cv2.imshow("Annotated", rgb_frame)
+    # quit if the user presses q
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
 
-odom.start()
-# odom.cam.start_display()
-
-t = Thread(target=target)
-t.start()
-
-input("Press Enter to continue...")
-
-STOPPED = True
-t.join()
-odom.stop()
+cam.stop()

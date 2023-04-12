a6c589714b5ea25951bf3e1d59db67944d3339ef
justincdavis <jcdavis@mssm.org>
2023-04-11 18:27:50 -0600

    lol pushed the broken threaded version

--- examples/oakd/odometer.py
+++ examples/oakd/odometer.py
@@ -11,15 +11,17 @@ STOPPED = False
 cam = OAK_Camera()
 cam.start(block=True)
 odom = OAK_Odometer(cam)
-odom.start()
 
 while True:
+    odom.update()
     rgb_frame = cam.rgb
-    drawPoseOnImage(odom.current_pose(), rgb_frame)
+    rgb_frame = cv2.resize(rgb_frame, (640, 480))
+    pose = odom.current_pose()
+    print(pose)
+    drawPoseOnImage(pose, rgb_frame)
     cv2.imshow("Annotated", rgb_frame)
     # quit if the user presses q
-    if cv2.waitKey(33) & 0xFF == ord("q"):
+    if cv2.waitKey(100) & 0xFF == ord("q"):
         break
 
-odom.stop()
 cam.stop()
